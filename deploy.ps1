565a740bb80873b2e188664a115876a1801f1282
notevensmart <parth.college123@gmail.com>
2025-07-13 17:41:10 +1000

    My latest deployment

--- deploy.ps1
+++ deploy.ps1
@@ -10,6 +10,6 @@ git push
 
 gcloud builds submit --tag us-central1-docker.pkg.dev/carboncoach-465605/carboncoach-repo/carboncoach
 
-gcloud run deploy carboncoach --image us-central1-docker.pkg.dev/carboncoach-465605/carboncoach-repo/carboncoach --platform managed --region us-central1 --allow-unauthenticated --set-env-vars "OPENROUTER_API_KEY=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,CLIMATIQ_API_KEY=SCCDV0RCQD1AV7RN7SA3P8RDAR"
+gcloud run deploy carboncoach --image us-central1-docker.pkg.dev/carboncoach-465605/carboncoach-repo/carboncoach --platform managed --region us-central1 --allow-unauthenticated --set-env-vars "OPENAI_API_KEY=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,CLIMATIQ_API_KEY=SCCDV0RCQD1AV7RN7SA3P8RDAR"
 
 
