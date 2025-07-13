bd697ad65673d5e99e8c213245f856951599e4ce
notevensmart <parth.college123@gmail.com>
2025-07-13 17:15:37 +1000

    My latest deployment

--- deploy.ps1
+++ deploy.ps1
@@ -8,9 +8,8 @@ git commit -m "$CommitMessage"
 
 git push
 
-docker build -t us-central1-docker.pkg.dev/carboncoach-465605/carboncoach-repo/carboncoach .
+gcloud builds submit --tag us-central1-docker.pkg.dev/carboncoach-465605/carboncoach-repo/carboncoach
 
-docker push us-central1-docker.pkg.dev/carboncoach-465605/carboncoach-repo/carboncoach
 gcloud run deploy carboncoach --image us-central1-docker.pkg.dev/carboncoach-465605/carboncoach-repo/carboncoach --platform managed --region us-central1 --allow-unauthenticated --set-env-vars "OPENROUTER_API_KEY=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,CLIMATIQ_API_KEY=SCCDV0RCQD1AV7RN7SA3P8RDAR"
 
 
