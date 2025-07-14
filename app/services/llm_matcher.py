7662f3659d98c493d42a2c22e5f2c292cfd460be
notevensmart <parth.college123@gmail.com>
2025-07-14 17:16:14 +1000

    My latest deployment

--- app/services/llm_matcher.py
+++ app/services/llm_matcher.py
@@ -12,7 +12,7 @@ load_dotenv(dotenv_path="key.env")  # Load OpenRouter key
 llm = ChatOpenAI(
     model="deepseek/deepseek-chat-v3-0324:free",
     base_url="https://openrouter.ai/api/v1",
-    api_key=os.getenv("OPENAI_API_KEY"),
+    api_key=os.getenv("OPENROUTER_API_KEY"),
     temperature=0
 )
 
